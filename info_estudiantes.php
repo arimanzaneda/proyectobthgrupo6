d14e22fea691cebdc3edc8164f3589752e3063cc
arimanzaneda <manzanedanunezvalentina@pedropoveda.edu.bo>
2025-09-13 01:36:55 -0400

    estilos estudiantes
    media

---
+++ info_estudiantes.php
@@ -0,0 +1,35 @@
+      .section-content {
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+  </style>
+</head>
+<body>
+  <div class="contenedor">
+    <div class="header">
+      <div class="header-info">
+        <h2>NOMBRE COMPLETO DEL ESTUDIANTE</h2>
+        <p>Estudiante</p>
+      </div>
+      <img src="https://st.depositphotos.com/1724125/1954/v/450/depositphotos_19547537-stock-illustration-cartoon-student.jpg" alt="Foto Estudiante" />
+    </div>
+    <div class="section">
+      <h3>Datos Básicos</h3>
+      <div class="section-content">
+        <div class="item"><span class="letra">CI:</span><p>12345678</p></div>
+        <div class="item"><span class="letra">Correo:</span><p>sdjsuhsz@gmail.com</p></div>
+        <div class="item"><span class="letra">Teléfono:</span><p>76543210</p></div>
+        <div class="item"><span class="letra">Dirección:</span><p>ubicacion</p></div>
+      </div>
+    </div>
+    <div class="section">
+      <h3>Datos Personales</h3>
+      <div class="section-content">
+        <div class="item"><span class="letra">Fecha de Nacimiento:</span><p>15/05/2007</p></div>
+        <div class="item"><span class="letra">Curso:</span><p>6to B</p></div>
+        <div class="item"><span class="letra">Rude:</span><p>71474834</p></div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
